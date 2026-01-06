93920bfa4fc47f6ff035f5a478044acd386f03df
Abdullah Sher Muhammad <147991640+AbdullahSherMuhammad@users.noreply.github.com>
2026-01-07 00:49:15 +0500

    Refactor data reference to use pgdata variable

--- src/app/industries/page.js
+++ src/app/industries/page.js
@@ -9,19 +9,22 @@ import Progress from "../../components/industries_pages/process_section"
 import Casestudy from "../../components/industries_pages/case_study"
 import Faqs from "../../components/industries_pages/faq"
 import Footer from "../../components/footer"
+
+const pgdata = data[0]
+
 export default function Home() {
 
   return (
     <>
     <Header/>
     <Hero />
-   <Introduction data={data.ai_apps_page.introduction_section} />
-   <KeyBenefitsSection data={data.ai_apps_page.key_benefits_section}/>
-   <WhatYouGetSection data={data.ai_apps_page.what_you_get_section}/>
-   <Technalogies data={data.ai_apps_page.technologies_section}/>
-   <Progress data={data.ai_apps_page.process_section}/>
-   <Casestudy data={data.ai_apps_page.case_study_section}/>
-   <Faqs data={data.ai_apps_page.faq_section}/>
+   <Introduction data={pgdata.ai_apps_page.introduction_section} />
+   <KeyBenefitsSection data={pgdata.ai_apps_page.key_benefits_section}/>
+   <WhatYouGetSection data={pgdata.ai_apps_page.what_you_get_section}/>
+   <Technalogies data={pgdata.ai_apps_page.technologies_section}/>
+   <Progress data={pgdata.ai_apps_page.process_section}/>
+   <Casestudy data={pgdata.ai_apps_page.case_study_section}/>
+   <Faqs data={pgdata.ai_apps_page.faq_section}/>
     <Footer/>
 
     
