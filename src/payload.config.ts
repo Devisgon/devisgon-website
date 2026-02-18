465177664ffca517d87af40e42d68d54a4268f2a
Ch Abdullah Shafique <abdullahshafique319@gmail.com>
2026-02-18 18:58:08 +0500

    Reduce max database connection pool size to 3

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -38,7 +38,7 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URL || "",
-      max: 10,
+      max: 3,
       ssl: { rejectUnauthorized: false },
     },
   }),
