4886cb7ad90ac0af2c55191dcb8e5fc771359d36
Ch Abdullah Shafique <abdullahshafique319@gmail.com>
2026-02-18 18:39:28 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,19 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Opt-in to the new Next 16 caching model (optional but recommended)
-  // cacheComponents: true, 
-
- 
+  // Adding this helps silence Turbopack/Webpack conflict warnings
+  turbopack: {}, 
+  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    // Next 16 increased the default cache TTL to 4 hours
-    minimumCacheTTL: 14400, 
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'test-omega-coral-10.vercel.app',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'test-omega-coral-10.vercel.app', pathname: '/**' },
+      { protocol: 'https', hostname: '**.supabase.co', pathname: '/**' },
     ],
   },
+
 };
 
 export default withPayload(nextConfig);
