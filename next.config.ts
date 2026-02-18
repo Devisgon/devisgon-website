df832596f2837485b969f150d54fb03fc1394f0f
Ch Abdullah Shafique <abdullahshafique319@gmail.com>
2026-02-18 17:11:05 +0500

    a

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,14 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Turbopack warning: Avoid complex plugins here while it's experimental
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'test-omega-coral-10.vercel.app',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'test-omega-coral-10.vercel.app' },
+      { protocol: 'https', hostname: '**.supabase.co' },
     ],
   },
 };
