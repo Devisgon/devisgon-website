f08fdbd0114ed5408532b0e2a58f2c410d3f7ae3
Ch Abdullah Shafique <abdullahshafique319@gmail.com>
2026-02-24 16:00:35 +0500

    Set default S3 region to 'us-east-1'
    Change default S3 region to 'us-east-1'.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -55,7 +55,7 @@ export default buildConfig({
       bucket: process.env.S3_BUCKET!,
       config: {
         endpoint: process.env.S3_ENDPOINT!,
-        region: process.env.S3_REGION || "auto",
+        region: "us-east-1",
         credentials: {
           accessKeyId: process.env.S3_ACCESS_KEY_ID!,
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
