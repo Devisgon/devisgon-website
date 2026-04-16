2b39457e2ae8372c60d8c3a362fa3fe83d9d4cda
abdullahdev30 <abdllahshafique319@gmail.com>
2026-04-16 12:37:29 +0500

    home page fixes

--- src/components/home_page/ceo_section.tsx
+++ src/components/home_page/ceo_section.tsx
@@ -1,4 +1,5 @@
 import { CEOSectionProps } from "@/types/homepage/ceo"
+import Image from "next/image";
 
 const CEOSection = ({ data }: CEOSectionProps) => {
   if (!data) return null;
@@ -9,11 +10,14 @@ const CEOSection = ({ data }: CEOSectionProps) => {
         <div className="relative  rounded-2xl shadow-[0px_85.26px_181.4px_0px_rgba(21, 21, 21, 0.15)] p-4 pt-8">
           <div className="flex justify-center -translate-y-10">
             <div className="w-68 md:w-96 h-84 rounded-xl overflow-hidden shadow-lg">
-              <img
-                src={data.image}
-                alt="CEO"
-                className="w-full h-full object-cover"
-              />
+            <Image
+  src="/home_page/ceo_section/ceo.webp"
+  alt="CEO"
+  width={442}
+  height={336}
+  sizes="(max-width: 768px) 100vw, 442px"
+  className="w-full h-full object-cover"
+/>
             </div>
           </div>
           
--- src/components/home_page/services_section.tsx
+++ src/components/home_page/services_section.tsx
@@ -27,12 +27,16 @@ export default function ServicesSection({ data }: ServicesSectionProps) {
     href={card.link}
     className={styles.card}
   >
-    <img
-      src={card.image_alt}
-      alt={card.title}
-      
-      className={styles.cardImage}
-    />
+   
+<Image
+   src={card.image_alt}
+   alt={card.title}
+  width={653}
+  height={500}
+  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 653px"
+        className={styles.cardImage}
+
+/>
 
      <div className={styles.overlay}>
       <h3 className={styles.title}>{card.title}</h3>
